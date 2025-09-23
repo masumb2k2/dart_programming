11597b7f8becc6951dc6e47cba280ba6264710f4
masumb2k2 <masumb2k2@gmail.com>
2025-09-23 16:54:34 +0600

    operator

--- lib/Day-03/2.concatination.dart
+++ lib/Day-03/2.concatination.dart
@@ -2,5 +2,5 @@ void main(){
 String firstName="Masum";
 String lastName="billah";
 String fullName= firstName + " " + lastName;//concatination
-print(fullName);
+print(fullName); 
 }
---
+++ lib/Day-03/3.operator.dart
@@ -0,0 +1,34 @@
+void main(){
+  int a =34;
+  int b=23;
+  //arithmetic operator
+  print(a+b); //addition
+  print(a-b); //subtraction
+  print(a*b); //multiplication
+  print(a/b); //division
+  print(a%b); //modulus'
+
+//unary operator
+  print(a++); //postfix -> first print then increment
+  print(++a); //prefix -> first increment then print
+  print(--a); //pre decrement
+  print(a--); //post decrement
+  
+//relational operator
+  print(a>b); //greater than
+  print(a<b); //less than
+  print(a>=b); //greater than or equal to
+  print(a<=b); //less than or equal to
+  print(a==b); //equal toqas
+  print(a!=b); //not equal to
+
+//logical operator
+  print(a<30 && b>30);
+  print(a<30 || b>30);
+
+//assignment operator
+  a *=45; // a=a*45
+
+
+
+}
