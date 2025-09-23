6c6ab411b93eda47495cbd04625e9d85fb3dd974
masumb2k2 <masumb2k2@gmail.com>
2025-09-23 15:44:55 +0600

    string_interpolation

---
+++ lib/Day-03/1.string_interpolation.dart
@@ -0,0 +1,10 @@
+void main(){
+String username="masum";
+String message="Hello everyone, greetings from $username result => ${1+2}";
+print(message);
+
+ double random=34.56;
+ print(random.toStringAsFixed(1));
+
+}
+
---
+++ lib/Day-03/2.concatination.dart
@@ -0,0 +1,4 @@
+void main(){
+String firstName="Masum";
+String lastName="billah";
+}
