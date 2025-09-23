b8dec08e1204b1202e3dec05d1c84dcc5b306da6
masumb2k2 <masumb2k2@gmail.com>
2025-09-24 00:59:30 +0600

    list

---
+++ lib/Day-03/1.list.dart
@@ -0,0 +1,31 @@
+
+//<> angle_brackets
+//{} curly_braces
+//[] square_brackets
+void main(){
+  List <String> listOfStudentName=['rahim','karim','jamal','kamal'];
+  print(listOfStudentName);
+  print(listOfStudentName[1]);
+  listOfStudentName.add("Masum");//add new item
+  print(listOfStudentName);
+  print(listOfStudentName.length);//length of list
+
+  print(listOfStudentName.isEmpty);//check list is empty or not
+  print(listOfStudentName.isNotEmpty);//check list is not empty
+
+  print(listOfStudentName.last);//last item of list
+  print(listOfStudentName.first);//first item of list
+
+  List <String> names=['ss','nizam'];
+  listOfStudentName.addAll(['salam','nizam']);//add multiple item
+  listOfStudentName.addAll(names);//add multiple item from another list
+  print(listOfStudentName);
+  listOfStudentName.remove("jamal");//remove item
+  print(listOfStudentName);
+
+  names.insert(0,'bi');//insert item at specific index
+  print(names);
+  names.insertAll(1, ['salam','nizam']);//insert multiple item at specific index
+  print(names);
+
+}
