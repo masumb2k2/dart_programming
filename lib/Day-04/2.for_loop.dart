c21442adb0e991f3b357bf7de01442c2e505df5c
masumb2k2 <masumb2k2@gmail.com>
2025-09-25 01:28:33 +0600

    for loop

---
+++ lib/Day-04/2.for_loop.dart
@@ -0,0 +1,17 @@
+void main(){
+  //for loop syntax
+  //for(initialization; condition; increment/decrement)
+  for(int i=0; i<100;i++){
+    
+    if (i==50){
+      break; //break statement
+      //continue; //continue statement
+    }
+
+    if(i==20){
+      continue; //continue statement
+    }
+    print('Purchase something $i');
+  }
+
+}
