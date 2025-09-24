bbee1b46d0cfb2233bacdeb558c7deda32e620eb
masumb2k2 <masumb2k2@gmail.com>
2025-09-24 18:20:05 +0600

    practice

--- lib/dart_programming.dart
+++ lib/dart_programming.dart
@@ -1,3 +1,34 @@
-// int calculate() {
-//   return 6 * 7;
-// }
+void main(){
+
+   int num1=10;
+   int num2=20;
+   double num3=12.55;
+   String name="Masum";
+   
+
+   print(name.toUpperCase());
+   print(name.toLowerCase());
+   print (name.length);
+
+   //typer conversiton
+   print(num3.runtimeType);
+   int num4=num3.toInt();
+   print(num4.runtimeType);
+
+   String str=num3.toString();
+   print(str.runtimeType);
+
+    String str2='12.66';
+    double num5=double.parse(str2);
+    print(num5.runtimeType);
+
+    //String interpolation
+    String firstName="Masum";
+    String lastName="Billah";
+    print("My name is $firstName $lastName");
+
+    print(num3.toStringAsFixed(1));
+
+
+
+}
