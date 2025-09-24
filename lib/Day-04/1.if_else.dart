6d738a2b71c685c69be319e633a4d9936ef533a6
masumb2k2 <masumb2k2@gmail.com>
2025-09-25 01:03:08 +0600

    if else statement

---
+++ lib/Day-04/1.if_else.dart
@@ -0,0 +1,14 @@
+void main(){
+  //if else statement
+  double weather =27.50;
+  if(weather<25){
+    print("Going for walk!");
+  }
+  else if(weather>=25 && weather<=30){
+    print("Going for Gym!");
+  }
+  else{
+    print("Stay at home!");
+  }
+
+}
