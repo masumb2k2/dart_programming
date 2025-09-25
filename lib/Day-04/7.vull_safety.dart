623a8f4ee88f96416aae379bef164d2422822d93
masumb2k2 <masumb2k2@gmail.com>
2025-09-25 15:30:56 +0600

    forced unwrapping

--- lib/Day-04/7.vull_safety.dart
+++ lib/Day-04/7.vull_safety.dart
@@ -1,5 +1,6 @@
 void main(){
   //null safety
+  //null operator ?/!
   int? a;
   int? b=null;//nullable variable
   print(a);
@@ -10,4 +11,9 @@ print(a);
 a=null;
 print(a);
 
+a=45;
+b=45;
+// forced unwrapping
+int result=a! + b!;//assure that a and b will not be null
+print(result);
 }
